524c6bb79db98138412779b47170ab07efb92a28
chazz <chazzbg@gmail.com>
2016-05-04 23:35:49 +0300

    demo safe standing

--- src/com/chazz/Main.java
+++ src/com/chazz/Main.java
@@ -2,29 +2,54 @@ package com.chazz;
 
 import com.chazz.hexapod.Hexapod;
 
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Hexapod hex = new Hexapod(true);
+    private static double getTime(){
+        Date d = new Date();
+        double sec = d.getTime()/1000;
+        double usec = ((d.getTime()%1000)*1e-6);
+        return (sec+usec);
 
-        double[] servoAngles = new double[18];
-        for (int ii=0; ii<18; ii++)
+    }
+    public static void main(String[] args) {
+
+        int ii;
+
+        Hexapod hex = new Hexapod();
+        double lasttime, dt, inittime;
+
+        inittime = Main.getTime();
+        System.out.println(inittime);
+        float[] servoAngles = new float[18];
+        for (ii=0; ii<18; ii++)
         {
             servoAngles[ii] = 90;
         }
+        hex.setServoAngle(servoAngles);
+        hex.setAngles();
 
+        hex.setSpeed(0.0f);
+        hex.setTurning(0.0f);
 
-        hex.setServoAngle(servoAngles).setAngles();
-        hex.setSpeed(0.0).setTurning(0.0);
-
-
+        lasttime = Main.getTime();
+        System.out.println(hex);
         hex.safeStand();
+        System.out.println(hex);
+        while (hex.isSsRunning()){
+            dt = (Main.getTime() - lasttime);
+//            System.out.println(dt);
+            hex.step((float) dt);
 
-        for(double t = 0.0; t < 35.0d; t+=0.5){
-            hex.step(t);
 
-            System.out.println(hex);
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println(hex);
 
 
 

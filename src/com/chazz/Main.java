033afb5d07a788cd66171068b36d1e5913602708
Dimitar Ilkov <dimitar@vconnect.dk>
2016-05-04 16:57:48 +0300

    lame try to implement some movement

--- src/com/chazz/Main.java
+++ src/com/chazz/Main.java
@@ -5,6 +5,28 @@ import com.chazz.hexapod.Hexapod;
 public class Main {
 
     public static void main(String[] args) {
-        Hexapod hex = new Hexapod();
+        Hexapod hex = new Hexapod(true);
+
+        double[] servoAngles = new double[18];
+        for (int ii=0; ii<18; ii++)
+        {
+            servoAngles[ii] = 90;
+        }
+
+
+        hex.setServoAngle(servoAngles).setAngles();
+        hex.setSpeed(0.0).setTurning(0.0);
+
+
+        hex.safeStand();
+
+        for(double t = 0.0; t < 35.0d; t+=0.5){
+            hex.step(t);
+
+            System.out.println(hex);
+        }
+
+
+
     }
 }
